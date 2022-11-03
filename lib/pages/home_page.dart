2a01c380a741540911e99c2de852fc59e2529ac3
Viet Anh <vietanh@Viets-MacBook-Pro.local>
2022-11-03 13:55:15 +0700

    fix branch

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_app/pages/page_branch1.dart';
-import 'package:flutter_app/pages/page_branch2.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,8 +9,8 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        HomeBranch1(),
-        HomeBranch2(),
+        // FirstBranch(),
+        // SecondBranch(),
       ],
     );
   }
