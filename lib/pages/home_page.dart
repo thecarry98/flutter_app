f7c4a51353c09b9a711378b36c4d5f1224275ec0
Viet Anh <vietanh@Viets-MacBook-Pro.local>
2022-11-03 14:00:20 +0700

    add second branch

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_app/pages/second_branch.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,7 +11,7 @@ class HomePage extends StatelessWidget {
     return Column(
       children: [
         // FirstBranch(),
-        // SecondBranch(),
+        SecondBranch(),
       ],
     );
   }
---
+++ lib/pages/second_branch.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SecondBranch extends StatelessWidget {
+  const SecondBranch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+      child: Text('second branch'),
+    ));
+  }
+}
